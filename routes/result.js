99490c399460a48d7811560a144ba05281338076
wangyh <771314194@qq.com>
2019-03-05 18:11:52 +0800

    3rd commit

--- routes/result.js
+++ routes/result.js
@@ -10,10 +10,25 @@ router.get('/get', async function(req, res, next) {
 })*/
 
 /* Search result. */
-router.get('/search', async function(req, res, next) {
-	var sql = 'select * from result where domain=? and program=?';
-	var data = await db.query(sql, [req.query.Domain, req.query.program]);
-	res.json({status:0, data:data});
+router.post('/search', async function(req, res, next) {
+	console.log(req.query);
+	var sql1 = 'select count(*) as count from result where program=?';
+	var sql2 = 'select * from result where program=?';
+	var params = [];
+	params.push(req.query.program);
+	if (req.query.Domain) {
+		sql1 += ' and domain=?';
+		sql2 += ' and domain=?';
+		params.push(req.query.Domain);
+	}
+	console.log(params);
+	var data1 = await db.query(sql1, params);
+	console.log(data1);
+	sql2 += ' order by tt desc';
+	sql2 += ' limit ? offset ?';
+	params.push(parseInt(req.query.pageSize), (req.query.currentPage-1)*req.query.pageSize);
+	var data2 = await db.query(sql2, params);
+	res.json({status:0, data:[data1[0].count, data2]});
 })
 
 /* Get result. */
@@ -21,10 +36,9 @@ router.post('/selected', async function(req, res, next) {
     var sql1 = 'select count(*) as count from result where program=?';
 	var sql2 = 'select * from result where program=?';
 	var params = [req.body.program];
+	console.log(params);
 	var data1 = await db.query(sql1, params);
-	console.log(data1);
-	sql2 += ' order by tt desc';
-	sql2 += ' limit 10';
+	sql2 += ' order by tt desc limit 10';
 	var data2 = await db.query(sql2, params);
 	console.log(data2);
 	res.json({status:0, data:[data1[0].count, data2]});
@@ -32,10 +46,18 @@ router.post('/selected', async function(req, res, next) {
 
 /* Get each page's contents. */
 router.post('/page', async function(req, res, next) {
+	console.log(req.body);
 	var sql1 = 'select count(*) as count from result where program=?';
 	var sql2 = 'select * from result where program=?';
-	var params = [req.body.program];
+	var params = [];
+	params.push(req.body.program);
+	if (req.body.Domain) {
+		sql1 += ' and domain=?';
+		sql2 += ' and domain=?';
+		params.push(req.body.Domain);
+	}
 	var data1 = await db.query(sql1, params);
+	console.log(data1);
 	sql2 += ' order by tt desc';
 	sql2 += ' limit ? offset ?';
 	params.push(parseInt(req.body.pageSize), (req.body.currentPage-1)*req.body.pageSize);
