1904d63872cac1a17a9ae373d7dfcb25200f45a3
wangyh <771314194@qq.com>
2019-03-19 17:56:58 +0800

    9th commit

--- routes/admin.js
+++ routes/admin.js
@@ -6,22 +6,19 @@ var db = require('./db');
 router.get('/get', async function(req, res, next) {
 	var sql = 'select * from program';
 	var data = await db.query(sql, []);	
-	//for(let i=0; i<data.length; i++) {
-	//	data[i].route = '/program?type=' + data[i].id;
-	//}
+	for( let i=0; i<data.length; i++ ) {
+		data[i].route = '/program?type=' + data[i].id;
+	}
 	console.log(data);
 	res.json({status:0, data:data});
 })
 
 /* Add menu. */
 router.post('/add', async function(req, res, next) {
-	var sql1 = 'select max(id) as max from program';
-	var data1 = await db.query(sql1, []);
-	console.log(data1[0].max);
-	var sql2 = 'insert into program(name, description, type, route) values(?,?,?,?)';
-	var params2 = [];
-	params2.push(req.body.name, req.body.description, data1[0].max+1, '/program?type='+(data1[0].max+1));
-	var data2 = db.query(sql2, params2);
+	var sql = 'insert into program(name, description) values(?,?)';
+	var params = [];
+	params.push(req.body.name, req.body.description);
+	var data = db.query(sql, params);
 	res.json({status:0, msg:'Add successfully.'});
 })
 
@@ -37,7 +34,7 @@ router.post('/edit', async function(req, res, next) {
 /* Delete menu. */
 router.post('/delete', async function(req, res, next) {
 	var sql1 = 'select * from domain where program=?';
-	var data1 = await db.query(sql1, [req.body.type]);
+	var data1 = await db.query(sql1, [req.body.id]);
 	if ( data1.length==0 ) {
 		var sql = 'delete from program where id=?';
 		var data = await db.query(sql, [req.body.id]);
